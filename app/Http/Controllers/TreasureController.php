4920871324dafca436ed3886765f8a32c1f967a2
treasure <402216351@qq.com>
2015-12-30 12:26:21 +0800

    success

--- app/Http/Controllers/TreasureController.php
+++ app/Http/Controllers/TreasureController.php
@@ -5,17 +5,22 @@ use \DB;
 
 class TreasureController extends Controller {
 
-	public $location_url = "http://testapi.reapal.com/mobile/portal" ;
-	//public $location_url = "api.reapal.com/mobile/portal"; 
-	public $merchant_id = "100000000011015" ;
-	public $seller_email = "820061154@qq.com" ;
+	//public $location_url = "http://testapi.reapal.com/mobile/portal" ;
+	public $location_url = "http://api.reapal.com/mobile/portal"; 
+	public $merchant_id = "100000000218171" ;
+	//public $merchant_id = "100000000011015" ;
+	public $seller_email = "540473825@qq.com" ;
+	//public $seller_email = "820061154@qq.com" ;
 	public $notify_url = "http://120.55.92.123/recharge_notify" ;
 	public $return_url = "http://120.55.92.123/recharge_return" ;
 	public $currency = "156" ;
-	public $use_key = "e977ade964836408243b5g2444848f7b39d09fb41c77ae2e327ffb16f905e117" ;
-	public $public_key = "/paykey/treasure_rb/itrus001.pem" ;
-	public $private_key = "/paykey/treasure_rb/itrus001_pri.pem" ;
-	public $ending_key = "6js27LZ9165CTNHV" ;
+	public $use_key = "0714g5028gg3a20dfaab3ae99367d071b3aaa2g5d2fb8b58ddgefgd41053a884" ;
+	//public $use_key = "e977ade964836408243b5g2444848f7b39d09fb41c77ae2e327ffb16f905e117" ;
+	public $public_key = "/paykey/treasure_rb/publickey.pem" ;
+	public $private_key = "/paykey/treasure_rb/privatekey.pem" ;
+	public $rb_public_key = "/paykey/treasure_rb/itrus001.pem" ;
+	public $rb_private_key = "/paykey/treasure_rb/itrus001_pri.pem" ;
+	public $ending_key = "" ;
 	
 	public function __construct()
 	{
@@ -28,6 +33,7 @@ class TreasureController extends Controller {
 	}
 
 	public function recharge(Request $request){
+		//dd("哈哈");
 		$data['merchant_id'] = $this->merchant_id ;
 		$data['order_no'] = time().rand(1000,9999);
 		$data['transtime'] = date("Y-m-d h:i:s") ;
@@ -48,6 +54,7 @@ class TreasureController extends Controller {
 		ksort($data); reset($data);
 		$data['sign'] = $this->getSign($data) ;
 		$check_resutl = $this->check_and_insert($data);
+		$this->ending_key = $this->generateAESKey();
 		$ending_data['merchant_id'] = $this->merchant_id;
 		$ending_data['data'] = $this->build_rb_data(json_encode($data),$this->ending_key) ;
 		$ending_data['encryptkey'] = $this->build_rb_key($this->ending_key);
@@ -67,7 +74,7 @@ class TreasureController extends Controller {
 			$log_add['type'] = "1" ;
 			$log_add['status'] = "0" ;
 			$log_add['payment'] = "67";
-			$log_add['money'] = $data['total_fee'];
+			$log_add['money'] = $data['total_fee']/100;
 			$log_add['fee'] = "0";
 			$log_add['remark'] = "treasure_recharge_".$data['total_fee'] ;
 			$log_add['addtime'] = time();
@@ -80,28 +87,32 @@ class TreasureController extends Controller {
 		dd(" he he da ! ! ! ");
 	}
 	
-	public function return_and_addbill($order_id){
-		$recharge_result = DB::select("select * from dql_account_recharge where trade_no = ? and status = ? ",[$order_id , 0]);
+	public function return_and_addbill($order_id,$result){
+		$recharge_result = DB::select("select * from dql_account_recharge where trade_no = ? ",[$order_id]);
 		if(!empty($recharge_result)){
-			$up_result = DB::update("update dql_account_recharge set status = 1 ,amount = money , verify_time = ? where trade_no = ? " , [time() , $order_id]);
-			$account_result = DB::select("select * from dql_account where user_id = ? " ,[$recharge_result[0]->user_id]);
-			if($up_result && !empty($account_result)){
-				$log_add['user_id'] = $recharge_result[0]->user_id;
-				$log_add['type'] = "recharge" ;
-				$log_add['flow'] = "in" ;
-				$log_add['total'] = $account_result[0]->total + $recharge_result[0]->money ; 
-				$log_add['money'] = $recharge_result[0]->money ; 
-				$log_add['use_money'] = $account_result[0]->use_money + $recharge_result[0]->money ;
-				$log_add['no_use_money'] = $account_result[0]->no_use_money ;
-				$log_add['collection'] = $account_result[0]->collection ; 
-				$log_add['remark'] = "充值{$log_add['money']}元成功" ; 
-				$log_add['addtime'] = time();
-				$log_add['addip'] = Request::server("REMOTE_ADDR");
-				$insert_log = DB::insert("insert into dql_account_log(user_id,type,flow,total,money,use_money,no_use_money,collection,remark,addtime,addip) values(?,?,?,?,?,?,?,?,?,?,?)" , array_values($log_add)) ;
-				if($insert_log){
-					$up_account = DB::update("update dql_account set total = ?, use_money = ? where user_id = ? ",[$log_add['total'],$log_add['use_money'],$log_add['user_id']]);
-					if($up_account){
-						return true;
+			if($recharge_result[0]->status == 1){
+				return true ;
+			}else{
+				$up_result = DB::update("update dql_account_recharge set status = 1 ,amount = money , verify_time = ? ,verify_remark = ? where trade_no = ? " , [time() ,$result , $order_id]);
+				$account_result = DB::select("select * from dql_account where user_id = ? " ,[$recharge_result[0]->user_id]);
+				if($up_result && !empty($account_result)){
+					$log_add['user_id'] = $recharge_result[0]->user_id;
+					$log_add['type'] = "recharge" ;
+					$log_add['flow'] = "in" ;
+					$log_add['total'] = $account_result[0]->total + $recharge_result[0]->money ; 
+					$log_add['money'] = $recharge_result[0]->money ; 
+					$log_add['use_money'] = $account_result[0]->use_money + $recharge_result[0]->money ;
+					$log_add['no_use_money'] = $account_result[0]->no_use_money ;
+					$log_add['collection'] = $account_result[0]->collection ; 
+					$log_add['remark'] = "充值{$log_add['money']}元成功" ; 
+					$log_add['addtime'] = time();
+					$log_add['addip'] = Request::server("REMOTE_ADDR");
+					$insert_log = DB::insert("insert into dql_account_log(user_id,type,flow,total,money,use_money,no_use_money,collection,remark,addtime,addip) values(?,?,?,?,?,?,?,?,?,?,?)" , array_values($log_add)) ;
+					if($insert_log){
+						$up_account = DB::update("update dql_account set total = ?, use_money = ? where user_id = ? ",[$log_add['total'],$log_add['use_money'],$log_add['user_id']]);
+						if($up_account){
+							return true;
+						}
 					}
 				}
 			}
@@ -110,13 +121,40 @@ class TreasureController extends Controller {
 	}
 	
 	public function recharge_notify(Request $request){
-		dd($request);
+		$return_keys = $this->rebuild_rb_key($request::get("encryptkey"));
+		$return_result = $this->rebuild_rb_data($request::get("data"),$return_keys);
+		$return_result_array = json_decode($return_result,true);
+		if(!empty($return_result_array)){
+			$flag = $this->return_and_addbill($return_result_array['order_no'],$return_result);
+			$message = "" ;
+			if($flag === true){
+				$message = "SUCCESS" ;
+			}else{
+				$message = "FALSE" ;
+			}
+			$script = "MESSAGE::{$message}";
+			echo $script;exit;
+		}
+		return false;
 	}
 	
 	public function recharge_return(Request $request){
-		$return_result = $this->rebuild_rb_data($request::get("data"),$this->ending_key);
-		//$return_result = $this->rebuild_rb_key($request::get("data"));
-		dd($return_result);
+		$return_keys = $this->rebuild_rb_key($request::get("encryptkey"));
+		//dd($return_keys);
+		$return_result = $this->rebuild_rb_data($request::get("data"),$return_keys);
+		$return_result_array = json_decode($return_result,true);
+		if(!empty($return_result_array)){
+			$flag = $this->return_and_addbill($return_result_array['order_no'],$return_result);
+			$message = "" ;
+			if($flag === true){
+				$message = "充值成功" ;
+			}else{
+				$message = "充值失败" ;
+			}
+			$script = "<script>alert('{$message}');window.location.href='http://www.jinzhangguinb.com/front_account/toMyAccount.dql?device=weixin'</script>";
+			echo $script;exit;
+		}
+		return false;
 	}
 	
 	public function build_rb_form($data){
@@ -130,29 +168,22 @@ class TreasureController extends Controller {
 	}
 	
 	public function rebuild_rb_data($treasure_data,$ending_key){
-		$td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
-		$iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-		//$decrypted= mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$ending_key,$treasure_data,MCRYPT_MODE_ECB,$iv);
-		//$dec_s = strlen($decrypted);
-		//$padding = ord($decrypted[$dec_s-1]);
-		//$decrypted = substr($decrypted, 0, -$padding);
-		mcrypt_generic_init($td,$ending_key,$iv);
-		$treasure_data = mdecrypt_generic($td,$treasure_data);
-		mcrypt_generic_deinit($td);
-		mcrypt_module_close($td);
-		dd($treasure_data);
+		$decrypted= mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$ending_key,base64_decode($treasure_data),MCRYPT_MODE_ECB);
+		$dec_s = strlen($decrypted);
+		$padding = ord($decrypted[$dec_s-1]);
+		$treasure_data = substr($decrypted, 0, -$padding);
 		return $treasure_data;
 	}
 	
 	public function rebuild_rb_key($treasure_key){
 		$private_key= file_get_contents(resource_path().$this->private_key);
 		$private_key_yes =  openssl_pkey_get_private($private_key);
-		openssl_private_decrypt($treasure_key,$return_keys,$private_key_yes);
+		$res = openssl_private_decrypt(base64_decode($treasure_key),$return_keys,$private_key_yes);
 		return $return_keys;
 	}
 	
 	public function build_rb_key($treasure_key){
-		$public_key = file_get_contents(resource_path().$this->public_key);
+		$public_key = file_get_contents(resource_path().$this->rb_public_key);
 		$public_key_yes = openssl_pkey_get_public($public_key);
 		openssl_public_encrypt($treasure_key,$return_keys,$public_key_yes);
 		return base64_encode($return_keys); 
